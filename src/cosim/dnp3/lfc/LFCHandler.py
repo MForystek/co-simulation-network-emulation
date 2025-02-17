cdfbda16710c36d8e5bc90b3f2941655c09d8cb7
MForystek <michal.forystek@kaust.edu.sa>
2025-02-17 23:42:20 +0300

    LFC handler moved from SOE handler to a separate class

---
+++ src/cosim/dnp3/lfc/LFCHandler.py
@@ -0,0 +1,128 @@
+import threading
+import time
+
+from cosim.mylogging import getLogger
+
+
+_log = getLogger(__name__, "logs/LFCHandler")
+
+
+class LFCHandler:
+    def __init__(self):   
+        self._timer_lock = threading.Lock()
+        self._vars_lock = threading.Lock()
+         
+        # LFC parameters
+        self._K_I = 0.005
+        self._base_rotor_speed = 377
+        self._beta = 20
+        
+        # LFC variables
+        self._reset_controller_vars()
+        
+        controller_reset_handler = threading.Thread(
+        target=self._reset_controller_when_no_connection, daemon=True)
+        controller_reset_handler.start()
+        
+    
+    def get_updated_ACEs(self, visitor_index_and_value):
+        self._reset_timer()
+        self._calculate_tie_lines(visitor_index_and_value)
+        self._calculate_ACEs_from_LFC(visitor_index_and_value)
+        return [self._ACE1_1, self._ACE2_1, self._ACE3_1]
+    
+    
+    def _reset_controller_vars(self):
+        self._vars_lock.acquire()
+        self._ACE1_1 = 0.0
+        self._ACE2_1 = 0.0
+        self._ACE3_1 = 0.0
+        self._integral1 = 0.0
+        self._integral2 = 0.0
+        self._integral3 = 0.0
+        self._prev_time = 0.0
+        self._tie_lines = [0.0, 0.0, 0.0]
+        self._vars_lock.release()
+    
+    
+    def _reset_controller_when_no_connection(self):
+        max_disconnection_time = 2
+        
+        while(True):
+            self._reset_timer()
+            while(self._timer < max_disconnection_time):
+                time.sleep(1)
+                self._increment_timer()
+            self._reset_controller_vars()
+            _log.info(f"No connection for {max_disconnection_time}. LFC handler reset.")
+ 
+    
+    def _reset_timer(self):
+        self._timer_lock.acquire()
+        self._timer = 0
+        self._timer_lock.release()
+        
+        
+    def _increment_timer(self):
+        self._timer_lock.acquire()
+        self._timer += 1
+        self._timer_lock.release()
+    
+    
+    def _calculate_tie_lines(self, visitor_index_and_value):
+        tl_13_T19 = visitor_index_and_value[10][1] # tie line from area 1 to 3, line number 19
+        tl_12_T21 = visitor_index_and_value[11][1] # the rest by analogy
+        tl_23_T05 = visitor_index_and_value[12][1] 
+        tl_23_T02 = visitor_index_and_value[13][1] 
+        tl_31_T19 = visitor_index_and_value[14][1]
+        tl_21_T21 = visitor_index_and_value[15][1]
+        tl_32_T05 = visitor_index_and_value[16][1]
+        tl_32_T02 = visitor_index_and_value[17][1]
+        
+        self._tie_lines[0] = (tl_13_T19 + tl_12_T21 - 143.442) / 100
+        self._tie_lines[1] = (tl_21_T21 + tl_23_T02 + tl_23_T05 - 247.21) / 100
+        self._tie_lines[2] = (tl_31_T19 + tl_32_T02 + tl_32_T05 + 389.5655) / 100
+        _log.info(f"TL 1: {self._tie_lines[0]} || TL 2: {self._tie_lines[1]} || TL 3: {self._tie_lines[2]}")
+    
+    
+    def _calculate_ACEs_from_LFC(self, visitor_index_and_value):
+        curr_time = time.time_ns()
+        if self._prev_time == 0:
+            self._set_prev_time(curr_time - 1)
+        time_diff_in_sec = (curr_time - self._prev_time) / 1_000_000_000
+        self._set_prev_time(curr_time)
+        
+        speed_6 = visitor_index_and_value[5][1] # for ACE1_1
+        self._update_LFC_controller(1, speed_6, time_diff_in_sec)
+        self._ACE1_1 = self._integral1 + 0.02857 #0.02957
+            
+        speed_1 = visitor_index_and_value[0][1] # for ACE2_1
+        self._update_LFC_controller(2, speed_1, time_diff_in_sec)
+        self._ACE2_1 = self._integral2 + 0.02574 #0.02974
+        
+        speed_3 = visitor_index_and_value[2][1] # for ACE3_1    
+        self._update_LFC_controller(3, speed_3, time_diff_in_sec)
+        self._ACE3_1 = self._integral3 + 0.02371 #0.01971
+        
+        _log.info(f"Gen 1 | Speed: {speed_1} || Gen 3 | Speed: {speed_3} || Gen 6 | Speed: {speed_6}")
+        _log.info(f"ACE1_1: {self._ACE1_1} || ACE2_1: {self._ACE2_1} || ACE3_1: {self._ACE3_1}")    
+                
+    
+    def _set_prev_time(self, new_time):
+        self._vars_lock.acquire()
+        self._prev_time = new_time
+        self._vars_lock.release()
+        
+        
+    def _update_LFC_controller(self, area_num, freq, time_diff):
+        error = (freq - self._base_rotor_speed) / self._base_rotor_speed * self._beta + self._tie_lines[area_num-1]
+        new_value = error * -self._K_I * time_diff
+        
+        self._vars_lock.acquire()
+        if area_num == 1:
+            self._integral1 += new_value
+        elif area_num == 2:
+            self._integral2 += new_value
+        elif area_num == 3:
+            self._integral3 += new_value
+        self._vars_lock.release()
--- src/cosim/dnp3/lfc/master.py
+++ src/cosim/dnp3/lfc/master.py
@@ -5,81 +5,24 @@ import time
 from pydnp3.opendnp3 import GroupVariation
 from dnp3_python.dnp3station.visitors import *
 
+from cosim.dnp3.lfc.LFCHandler import LFCHandler
 from cosim.dnp3.soe_handler import SOEHandler
 from cosim.dnp3.master import MasterStation
 
 
-class LFCSOEHandler(SOEHandler):
+class IEEE39BusSOEHandler(SOEHandler):
     def __init__(self, log_file_path="logs/soehandler.log", soehandler_log_level=logging.INFO, station_ref=None, *args, **kwargs):
         super().__init__(log_file_path, soehandler_log_level, station_ref, *args, **kwargs)
-        self._integral1 = 0.0
-        self._integral2 = 0.0
-        self._integral3 = 0.0
-        self._ACE1 = 0.0
-        self._ACE2 = 0.0
-        self._ACE3 = 0.0
-        self.prev_time = 0.0
+        self._LFC_handler = LFCHandler()
     
     
-    def _process_incoming_data(self, info_gv, visitor_ind_val):
-        if info_gv in [GroupVariation.Group30Var6]:
-            curr_time = time.time_ns()
-            if self.prev_time == 0:
-                self.prev_time = curr_time - 1
-            time_diff_in_sec = (curr_time - self.prev_time) / 1_000_000_000
-            self.prev_time = curr_time
-            
-            K_I = 0.005
-            
-            speed_1 = visitor_ind_val[0][1] # for ACE2_1
-            speed_3 = visitor_ind_val[2][1] # for ACE3_1
-            speed_6 = visitor_ind_val[5][1] # for ACE1_1
-            
-            tl_13_T19 = visitor_ind_val[10][1] # tie line from area 1 to 3, line number 19
-            tl_12_T21 = visitor_ind_val[11][1] # the rest by analogy
-            tl_23_T05 = visitor_ind_val[12][1] 
-            tl_23_T02 = visitor_ind_val[13][1] 
-            tl_31_T19 = visitor_ind_val[14][1]
-            tl_21_T21 = visitor_ind_val[15][1]
-            tl_32_T05 = visitor_ind_val[16][1]
-            tl_32_T02 = visitor_ind_val[17][1]
-            
-            tl1 = (tl_13_T19 + tl_12_T21 - 143.442) / 100
-            tl2 = (tl_21_T21 + tl_23_T02 + tl_23_T05 - 247.21) / 100
-            tl3 = (tl_31_T19 + tl_32_T02 + tl_32_T05 + 389.5655) / 100
-            
-            self.logger.info(f"Gen 1 | Speed: {speed_1} || Gen 3 | Speed: {speed_3} || Gen 6 | Speed: {speed_6}")
-            self.logger.info(f"TL 1: {tl1} || TL 2: {tl2} || TL 3: {tl3}")
-            
-            self._update_controller(1, speed_6, tl1, K_I, time_diff_in_sec)
-            ACE1_1 = self._integral1 + 0.02857 #0.02957
-            
-            self._update_controller(2, speed_1, tl2, K_I, time_diff_in_sec)
-            ACE2_1 = self._integral2 + 0.02574 #0.02974
-            
-            self._update_controller(3, speed_3, tl3, K_I, time_diff_in_sec)
-            ACE3_1 = self._integral3 + 0.02371 #0.01971
-            
-            self.logger.info(f"ACE1_1: {ACE1_1} || ACE2_1: {ACE2_1} || ACE3_1: {ACE3_1}")
-            
-            self.station_ref.send_direct_point_command(40, 4, 0, ACE1_1)
-            self.station_ref.send_direct_point_command(40, 4, 1, ACE2_1)
-            self.station_ref.send_direct_point_command(40, 4, 2, ACE3_1)
-        
+    def _process_incoming_data(self, info_gv, visitor_index_and_value):
+        if info_gv in [GroupVariation.Group30Var6]:            
+            ACEs = self._LFC_handler.get_updated_ACEs(visitor_index_and_value)
+            self.station_ref.send_direct_point_command(40, 4, 0, ACEs[0])
+            self.station_ref.send_direct_point_command(40, 4, 1, ACEs[1])
+            self.station_ref.send_direct_point_command(40, 4, 2, ACEs[2])
 
-    def _update_controller(self, area_num, freq, tie_lines, K_I, time_diff):
-        base_rotor_speed = 377
-        beta = 20
-        
-        error = (freq - base_rotor_speed) / base_rotor_speed * beta + tie_lines
-        update_integral = error * -K_I * time_diff
-        if area_num == 1:
-            self._integral1 += update_integral
-        elif area_num == 2:
-            self._integral2 += update_integral
-        elif area_num == 3:
-            self._integral3 += update_integral
-     
         
 def main():
     logs_file = "logs/d_r_lfc_master.log"
@@ -87,10 +30,11 @@ def main():
     port = 20002
     
     master = MasterStation(outstation_ip=outstation_ip, port=port, master_id=1, outstation_id=2)
-    soe_handler = LFCSOEHandler(logs_file, station_ref=master)
+    soe_handler = IEEE39BusSOEHandler(logs_file, station_ref=master)
     master.configure_master(soe_handler, outstation_ip, port)
     
-    threading.Thread(target=master.start(), daemon=True)
+    master_thread = threading.Thread(target=master.start, daemon=True)
+    master_thread.start()
     try:
         while True:
             time.sleep(1)
